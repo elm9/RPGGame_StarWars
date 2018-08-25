17dd5d5bbfe79aa312554ef8e8828be754fdda7a
em <6em.rva@gmail.com>
2018-08-25 14:16:54 -0400

    added psudocode, reset css file content, and linked files

---
+++ assets/javascript/game.js
@@ -0,0 +1,36 @@
+
+
+// variables
+
+    //characters {name: "", img: "", hp: "", ap: "", ca: ""}
+
+    //chosen player
+    //chosen enemy 
+
+// display characters in starting area
+
+
+// choose player
+
+
+// choose enemy
+
+
+    // attack button
+        // check if player and enemy have been chosen, if so:
+            // player attacks enemy == (enemy HP) - (player AP)
+            // enemy counter attacks player == (player HP) - (enemy CA)
+            // increase player's AP by base AP
+            // if enemy's HP < or = 0 remove character and choose new enemy
+            // player wins by defeating all enemies 
+                // restart button appears
+            // player loses if HP < or = 0
+                // restart button appears
+        //if player is chosen, enemy is not chosen, and there are enemies still alive:
+            // please choose an enemy! notification
+
+
+    // restart button
+    
+
+//start game function
